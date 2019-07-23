1c93f2acde47f5ee20618eb52fb05c9e4587294c
shojol <dip_shojol@yahoo.com>
2019-07-23 17:02:38 -0400

    Updates

---
+++ precache-manifest.032d1045720897b9a85a4b151f980ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f242ba70326a6bfddae63c81e5b04bdc",
+    "url": "/react-color-palette/index.html"
+  },
+  {
+    "revision": "65d984149aad7390ca4e",
+    "url": "/react-color-palette/static/css/main.56026a3e.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-color-palette/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "65d984149aad7390ca4e",
+    "url": "/react-color-palette/static/js/main.c0e2fecc.chunk.js"
+  },
+  {
+    "revision": "4bf4f10fbfea94954f90",
+    "url": "/react-color-palette/static/js/runtime~main.27d239de.js"
+  },
+  {
+    "revision": "2b56778a5a5efba40b3bc3c7ac39c6aa",
+    "url": "/react-color-palette/static/media/logo.2b56778a.gif"
+  }
+]);
